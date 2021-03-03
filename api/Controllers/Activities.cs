9d7521ca568beabd94b67ed94af887f0cd6575de
mdip32 <mattdipietro32@gmail.com>
2021-03-02 21:23:48 -0500

    Initial commit

---
+++ api/Controllers/Activities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using persistence;
+
+namespace api.Controllers
+{
+    public class Activities : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public Activities(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+        
+    }
+}
---
+++ persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
