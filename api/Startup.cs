025a5751918516f5b87c7a7da6a78e17b38e7066
mdip32 <mattdipietro32@gmail.com>
2021-03-03 19:40:07 -0500

    Added Semantic-UI to project

--- api/Startup.cs
+++ api/Startup.cs
@@ -34,6 +34,13 @@ namespace api
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
@@ -53,6 +60,8 @@ namespace api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
